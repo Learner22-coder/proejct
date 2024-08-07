e080666562bd6eb50236aa165a61fa7830b34147
Cheena <cheena@Kanaks-MacBook-Pro.local>
2024-08-07 20:49:34 +0530

    Introduction to JS

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // this is a demo js file
+
+// pie = 3.14;
+
+// var a;
+// var A;
+
+// // js object
+// const js = {
+//     asdfg: "fghj"
+// }
+
+// if(){
+
+// } else{
+
+// }
+
+// switch
+
+
+// variables: 
+// 1. var => var test;
+// 2. let => let test;
+// 3. const => const test;
+
+// try catch switch if else 
+
+// declaring -> creating a variable; var test
+// initialising -> setting the default value; test = 0
+
+
+// int test = 'string';
+
+// numbers, string, NAN, array, object, boolean; 
+
+/*
+
+sdfghj dfghjkvbnm
+
+*/
+
+var TEST = 78;
+var test = 45;
+
+console.log(TEST);
+console.log(test);
